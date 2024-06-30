6c2989375fa9a0b6bcec584788ce189e7b0de03c
Luz <luzojeda@proton.me>
2024-06-29 21:21:03 -0300

    add pictures

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,33 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		tags: z.array(z.string()).optional(),
-		draft: z.boolean().default(false),
-	}),
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        tags: z.array(z.string()).optional(),
+        draft: z.boolean().default(false),
+    }),
 });
 
 const microblog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		pubDate: z.coerce.date(),
-	}),
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.coerce.date(),
+    }),
 });
 
-export const collections = { blog, microblog };
+const pictures = defineCollection({
+    type: "data",
+    schema: ({ image }) =>
+        z.object({
+            en: z.object({ title: z.string(), description: z.string().optional() }),
+            es: z.object({ title: z.string(), description: z.string().optional() }),
+            pubDate: z.coerce.date(),
+            image: image(),
+        }),
+});
+
+export const collections = { blog, microblog, pictures };
