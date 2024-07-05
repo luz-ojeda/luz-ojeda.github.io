9365077690e52f91c0c1530eaef80651be8deeb0
Luz <luzojeda@proton.me>
2024-07-05 10:49:05 -0300

    make date post tables containers scroll on overflow x

--- src/components/react/DateComponent.jsx
+++ src/components/react/DateComponent.jsx
@@ -28,7 +28,7 @@ export default function DateComponent() {
             } else {
                 // Handle UNIX timestamps from input since they come as string
                 if (!isNaN(Number(debouncedDate))) {
-                    newDate = new Date(parseInt(debouncedDate))
+                    newDate = new Date(parseInt(debouncedDate));
                 } else {
                     newDate = new Date(debouncedDate);
                 }
@@ -51,182 +51,193 @@ export default function DateComponent() {
                 {invalidDate && <p className="error">The date entered was invalid</p>}
             </div>
             <h3 id="timestamp">Timestamp</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>method</th>
-                        <th>output</th>
-                        <th>description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">getTime()</th>
-                        <td>{date.getTime()}</td>
-                        <td>milliseconds since 1970-01-01, negative if before that date</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div style={{ overflowX: "auto" }}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>method</th>
+                            <th>output</th>
+                            <th>description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">getTime()</th>
+                            <td>{date.getTime()}</td>
+                            <td>milliseconds since 1970-01-01, negative if before that date</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
             <h3 id="local-time">Local Time Methods</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>method</th>
-                        <th>output</th>
-                        <th>description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">getDate()</th>
-                        <td>{date.getDate()}</td>
-                        <td>day of the month</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getDay()</th>
-                        <td>{date.getDay()}</td>
-                        <td>day of the week (0-6) where 0 is sunday</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getFullYear()</th>
-                        <td>{date.getFullYear()}</td>
-                        <td>four-digit year</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getMonth()</th>
-                        <td>{date.getMonth()}</td>
-                        <td>month (0-11)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getHours()</th>
-                        <td>{date.getHours()}</td>
-                        <td>hour (0-23)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getMinutes()</th>
-                        <td>{date.getMinutes()}</td>
-                        <td>minutes (0-59)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getSeconds()</th>
-                        <td>{date.getSeconds()}</td>
-                        <td>seconds (0-59)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getMilliseconds()</th>
-                        <td>{date.getMilliseconds()}</td>
-                        <td>milliseconds (0-999)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getTimezoneOffset()</th>
-                        <td>{date.getTimezoneOffset()}</td>
-                        <td>difference between UTC timezone and local time in minutes</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div style={{ overflowX: "auto" }}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>method</th>
+                            <th>output</th>
+                            <th>description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">getDate()</th>
+                            <td>{date.getDate()}</td>
+                            <td>day of the month</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getDay()</th>
+                            <td>{date.getDay()}</td>
+                            <td>day of the week (0-6) where 0 is sunday</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getFullYear()</th>
+                            <td>{date.getFullYear()}</td>
+                            <td>four-digit year</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getMonth()</th>
+                            <td>{date.getMonth()}</td>
+                            <td>month (0-11)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getHours()</th>
+                            <td>{date.getHours()}</td>
+                            <td>hour (0-23)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getMinutes()</th>
+                            <td>{date.getMinutes()}</td>
+                            <td>minutes (0-59)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getSeconds()</th>
+                            <td>{date.getSeconds()}</td>
+                            <td>seconds (0-59)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getMilliseconds()</th>
+                            <td>{date.getMilliseconds()}</td>
+                            <td>milliseconds (0-999)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getTimezoneOffset()</th>
+                            <td>{date.getTimezoneOffset()}</td>
+                            <td>difference between UTC timezone and local time in minutes</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
 
             <h3 id="utc">UTC Methods</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>method</th>
-                        <th>output</th>
-                        <th>description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">getUTCDate()</th>
-                        <td>{date.getUTCDate()}</td>
-                        <td>UTC day of the month</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getUTCDay()</th>
-                        <td>{date.getUTCDay()}</td>
-                        <td>UTC day of the week (0-6)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getUTCFullYear()</th>
-                        <td>{date.getUTCFullYear()}</td>
-                        <td>UTC four-digit year</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getUTCHours()</th>
-                        <td>{date.getUTCHours()}</td>
-                        <td>UTC hour (0-23)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getUTCMinutes()</th>
-                        <td>{date.getUTCMinutes()}</td>
-                        <td>UTC minutes (0-59)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getUTCMonth()</th>
-                        <td>{date.getUTCMonth()}</td>
-                        <td>UTC month (0-11)</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">getUTCSeconds()</th>
-                        <td>{date.getUTCSeconds()}</td>
-                        <td>UTC seconds (0-59)</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div style={{ overflowX: "auto" }}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>method</th>
+                            <th>output</th>
+                            <th>description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">getUTCDate()</th>
+                            <td>{date.getUTCDate()}</td>
+                            <td>UTC day of the month</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getUTCDay()</th>
+                            <td>{date.getUTCDay()}</td>
+                            <td>UTC day of the week (0-6)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getUTCFullYear()</th>
+                            <td>{date.getUTCFullYear()}</td>
+                            <td>UTC four-digit year</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getUTCHours()</th>
+                            <td>{date.getUTCHours()}</td>
+                            <td>UTC hour (0-23)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getUTCMinutes()</th>
+                            <td>{date.getUTCMinutes()}</td>
+                            <td>UTC minutes (0-59)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getUTCMonth()</th>
+                            <td>{date.getUTCMonth()}</td>
+                            <td>UTC month (0-11)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">getUTCSeconds()</th>
+                            <td>{date.getUTCSeconds()}</td>
+                            <td>UTC seconds (0-59)</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
 
             <h3 id="formatting">Formatting</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>method</th>
-                        <th>output</th>
-                        <th>description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">toDateString()</th>
-                        <td>{date.toDateString()}</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">toISOString()</th>
-                        <td>{date.toISOString()}</td>
-                        <td>
-                            simplified format based on{" "}
-                            <a href="https://en.wikipedia.org/wiki/ISO_8601">ISO 8601</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">toLocaleString()</th>
-                        <td>{date.toLocaleString()}</td>
-                        <td>language-sensitive representation of the date in the local timezone</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">toLocaleDateString()</th>
-                        <td>{date.toLocaleDateString()}</td>
-                        <td>same as before only date</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">toLocaleTimeString()</th>
-                        <td>{date.toLocaleTimeString()}</td>
-                        <td>same as before only time</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">toTimeString()</th>
-                        <td>{date.toTimeString()}</td>
-                        <td>time portion of the date interpreted in the local timezone</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">toUTCString()</th>
-                        <td>{date.toUTCString()}</td>
-                        <td>
-                            <a href="https://datatracker.ietf.org/doc/html/rfc7231#section-7.1.1.1">
-                                RFC 7231 format
-                            </a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div style={{ overflowX: "auto" }}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>method</th>
+                            <th>output</th>
+                            <th>description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">toDateString()</th>
+                            <td>{date.toDateString()}</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">toISOString()</th>
+                            <td>{date.toISOString()}</td>
+                            <td>
+                                simplified format based on{" "}
+                                <a href="https://en.wikipedia.org/wiki/ISO_8601">ISO 8601</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">toLocaleString()</th>
+                            <td>{date.toLocaleString()}</td>
+                            <td>
+                                language-sensitive representation of the date in the local timezone
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">toLocaleDateString()</th>
+                            <td>{date.toLocaleDateString()}</td>
+                            <td>same as before only date</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">toLocaleTimeString()</th>
+                            <td>{date.toLocaleTimeString()}</td>
+                            <td>same as before only time</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">toTimeString()</th>
+                            <td>{date.toTimeString()}</td>
+                            <td>time portion of the date interpreted in the local timezone</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">toUTCString()</th>
+                            <td>{date.toUTCString()}</td>
+                            <td>
+                                <a href="https://datatracker.ietf.org/doc/html/rfc7231#section-7.1.1.1">
+                                    RFC 7231 format
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
