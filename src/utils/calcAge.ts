98b980c1485854b945563a5bebcad82cb9be7d70
Luz <luzojeda@proton.me>
2024-07-04 22:43:53 -0300

    javascript dates post

--- src/utils/calcAge.ts
+++ src/utils/calcAge.ts
@@ -1,11 +1,20 @@
 export const calcAge = (function () {
-    const now = new Date()
-    const birthDate = new Date(1994, 0, 1);
-    let age = now.getFullYear() - birthDate.getFullYear()
+    const now = new Date();
+    const birthDate = new Date(1994, 0, 6);
+
+    let age = now.getFullYear() - birthDate.getFullYear();
 
     // Calculate if birthday has passed in the current year
-    const month = now.getMonth() - birthDate.getMonth()
-    if(month < 0) age--;
+    const currentMonth = now.getMonth();
+    const bdayMonth = birthDate.getMonth();
+
+    const birthdayNotHappenedYet =
+        (currentMonth === bdayMonth && now.getDate() < birthDate.getDate()) ||
+        bdayMonth > currentMonth;
+
+    if (birthdayNotHappenedYet) {
+        age--;
+    }
 
     return age;
 })();
