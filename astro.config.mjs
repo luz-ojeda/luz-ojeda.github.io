f3bd8f2ab842195f039f2755a2efa03cca89eeb1
synthopia <luzojeda@proton.me>
2024-12-10 19:24:56 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.luzojeda.com",
+	site: "https://www.lojeda.com",
 	integrations: [mdx(), sitemap(), react()],
 	i18n: {
 		defaultLocale: "en",
