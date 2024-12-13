18a0466e38c631826b591e1047e2717355a5e100
synthopia <luzojeda@proton.me>
2024-12-12 22:12:08 -0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "Luz Ojeda";
+export const SITE_TITLE = "a human";
 export const SITE_DESCRIPTION =
-	"The personal web page of Luz Ojeda, a web developer, sometimes cook and bookworm from Buenos Aires, Argentina";
+	"The personal web page of a web developer, sometimes cook and bookworm from Buenos Aires, Argentina";
