1313ee6721ebe02b868d3c6750db6d21a221625f
Luz <luzojeda@proton.me>
2025-01-05 20:55:45 -0300

    my name

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "a human";
+export const SITE_TITLE = "Lucas Ezequiel Ojeda";
 export const SITE_DESCRIPTION =
 	"The personal web page of a web developer, sometimes cook and bookworm from Buenos Aires, Argentina";
